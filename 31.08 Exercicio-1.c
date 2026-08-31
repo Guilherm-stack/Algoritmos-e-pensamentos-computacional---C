aca371547c9496af537736369fa8b1b130cedb23
Guilherm-stack <gf79359@gmail.com>
2026-08-31 19:45:30 -0300

    Update and rename dia 24 to 31.08 Exercicio-1.c

---
+++ 31.08 Exercicio-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int resposta,resposta2,total;
+    
+    
+    
+    
+    
+    printf("Digite quantidade recebida pela Manha :");
+    scanf("%d",&resposta);
+    printf("Digite qunatidade recebida pela Tarde :");
+    scanf("%d",&resposta2);
+    total = resposta + resposta2;
+    printf("Total do dia :%d",total);
+    return 0;
+}
